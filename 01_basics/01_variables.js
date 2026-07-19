a05a40febd6d21151b41d1813645bdf3f46fb803
Piyush Kumar Sharma <piyushsharma1817@gmail.com>
2026-07-19 08:30:38 +0000

    understood variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12345;  //
+let accountEmail = "piyush@google.com";
+let accountPassWord = "12345"    
+//use let instead of var because of because of issue in block scope and functional scope
+accountCity = "Delhi"  //also valid
+let accountState
+console.table([accountId,accountEmail,accountPassWord,accountCity,accountState])
